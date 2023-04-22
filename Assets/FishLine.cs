4ce12ed65c0e869ce3afef9c9d2b11d43b55598e
MayFlyOvO <ckmiaojapan@gmail.com>
2023-04-22 12:24:50 +0900

    Fish Line Update

---
+++ Assets/FishLine.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishLine : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float maxLength;
+    public float minLength;
+
+    private Obi.ObiRope rope;
+    private Obi.ObiRopeCursor cursor;
+
+    private float addLength = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rope = GetComponent<Obi.ObiRope>();
+        cursor = GetComponent<Obi.ObiRopeCursor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        addLength += Input.mouseScrollDelta.y * speed * Time.deltaTime;
+        if (addLength != 0)
+        {
+            var length = Mathf.Clamp(addLength + GetCurrentLength(), minLength, maxLength);
+            cursor.ChangeLength(length);
+            addLength = 0;
+        }
+    }
+
+    float GetCurrentLength()
+    {
+        return rope.restLength;
+    }
+
+    void SetLength(float value)
+    {
+        cursor.ChangeLength(value);
+    }
+}
