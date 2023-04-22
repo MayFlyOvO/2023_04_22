83afcce510879ff16b884033ecd9f3691a2e2bbd
古市 <galeos7@yahoo.co.jp>
2023-04-22 12:47:39 +0900

    衝突SE追加

--- Assets/Wall.cs
+++ Assets/Wall.cs
@@ -1,17 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Wall : MonoBehaviour
 {
     [SerializeField] GameObject UI;
 
+    [SerializeField]
+    AudioClip impuctSE;
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Stone")
         {
+            audioSource.PlayOneShot(impuctSE);
+
             Invoke("Clear", 3.0f);
             transform.position = new(100.0f, 100.0f, 0);
+
+            
         }
     }
 
